95d93f4cb87b2f0fa42e6ba8810e8e8578b00309
Pauli Varelius <pauli.varelius@gmail.com>
2016-06-14 19:12:00 +0300

    bug fixes

--- public/javascripts/googleMap.js
+++ public/javascripts/googleMap.js
@@ -1,8 +1,23 @@
+var mapStyle = [
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "gamma": 0.5
+            }
+        ]
+    }
+];
 
 var issLat = 0;
 var issLon = 0;
 var map;
 var marker;
+var myPosMarker
 
 window.onload = function() {
 	
@@ -19,23 +34,32 @@ function getData() {
 		$('#lon').text('Longitude: ' + result.longitude);
 		$('#alt').text('Altitude: ' + result.altitude + ' km');
 		$('#vel').text('Velocity: ' + result.velocity + ' km/h');
+    $('#vis').text('Visibility: ' + result.visibility);
   });
 	marker.setPosition({lat: issLat, lng: issLon});
 	map.setCenter(new google.maps.LatLng(issLat, issLon));
 	nite.refresh();
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        myPosMarker.setPosition({lat: position.coords.latitude, lng: position.coords.longitude});
+    });
 }
 
 
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          /*center: {lat: -34.397, lng: 150.644},*/
-          zoom: 3,
-          mapTypeId: google.maps.MapTypeId.HYBRID
-        });
+  var myOptions = {
+            draggable : false,
+            zoom : 3,
+            mapTypeId : google.maps.MapTypeId.TERRAIN,
+            disableDefaultUI: true,
+            styles: mapStyle
+            };
 
-        nite.init(map);
+  map = new google.maps.Map(document.getElementById('map'), myOptions);
+  nite.init(map);
 
     var icon = '/images/satellite-icon-gold.png';
+    var yourPosIcon = '/images/your-position.png';
 
     marker = new google.maps.Marker({
     /*position: {lat: -34.397, lng: 150.644},*/
@@ -44,4 +68,11 @@ function initMap() {
     icon: icon,
     label: 'ISS'
     });
+
+    myPosMarker = new google.maps.Marker({
+        map: map,
+        title: "You are here",
+        icon: yourPosIcon,
+    });
   }
+
